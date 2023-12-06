fdf7a3baf6cd2e8f092c07fb1fbe9361320d81f3
colas proust <colasproust@outlook.fr>
2023-12-06 10:28:40 +0100

    setting up for deployment

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+const DBURI = process.env.MONGODB_URI || process.env.DB_URI;
 const routes = require('./routes/routes')
 const cookieParser = require('cookie-parser')
 const { requireAuth, checkUser }= require('./middlewares/authMiddleware')
 
 
 // LES MIDDLEWARES ON VERRA PLUS TARD
-
 
 
 /* const { requireAuth, checkUser }= require('./middlewares/authMiddleware') */
@@ -16,23 +17,25 @@ const app = express();
 
 
 // middleware
-app.use(express.static('public'));
+/* app.use(express.static('public')); */
 app.use(express.json());
 app.use(cookieParser());
 
-// view engine
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // database connection
 const dbURI = 'mongodb+srv://colas:avaT8sp70QPVOK8W@cluster0.mfbymrg.mongodb.net/jobtracker';
 mongoose.connect(dbURI)
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(PORT))
     .catch((err) => console.log(err));
 
 // routes
 app.get('*', checkUser);
-/* app.get('/', (req, res) => res.render('home')); */
+/* app.get('/', (req, res) => res.redirect('home')); */
 app.use(routes);
 
 //cookies
 
+module.exports = app
