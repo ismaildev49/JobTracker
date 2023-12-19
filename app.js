e5d7e195b16e5e8169156908b4a60f03ee801e3d
ismaildev49 <ismaelbentatou@gmail.com>
2023-12-19 13:39:15 +0100

    ready to deploy

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3001;
-const DBURI = process.env.MONGODB_URI || process.env.DB_URI;
+const dbURI  = process.env.MONGODB_URI || process.env.DB_URI;
 const routes = require('./routes/routes')
 const cookieParser = require('cookie-parser')
 const upload = require("./uploads/multer");
@@ -30,22 +31,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // database connection
-const dbURI = 'mongodb+srv://ismaelbentatou:blabla123@cluster0.pnvb5it.mongodb.net/jobTracker';
+
 mongoose.connect(dbURI)
-    .then(() => {
-        console.log('MongoDB connected successfully');
-        return app.listen(PORT);
-    })
-    .then(() => {
-        console.log(`App is listening on port ${PORT}`);
-    })
-    .catch((err) => console.error(err));
+  .then(() => app.listen(PORT, (err, data) => {
+    console.log('listening to port ' + PORT);
+  }))
+  .catch((err) => console.log(err));
 // routes
 app.use(upload.fields([{ name: 'profilePicture', maxCount: 1 }, { name: 'cv', maxCount: 1 }]));
 
 app.get('/', (req, res) => res.redirect('dashboard'));
 app.use(routes);
 
-//cookies
+
 
 module.exports = app
