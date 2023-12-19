72474a0dfe28d411069fd07aed06a85839b23f43
ismaildev49 <ismaelbentatou@gmail.com>
2023-12-19 14:44:13 +0100

    ...

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Offer = require("../models/offer");
 const { isEmail } = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
