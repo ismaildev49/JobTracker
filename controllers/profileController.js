dd1c3e1af0bcd80f31d17c06946e62beb4bf452d
ismaildev49 <ismaelbentatou@gmail.com>
2023-12-21 10:22:35 +0100

    work correctly in local

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -24,8 +24,8 @@ module.exports.profile_update = async (req, res) => {
     console.log("got token");
     jwt.verify(token, "crazy secret secret", async (err, decodedToken) => {
       if (err) {    
-        res.status(400).json({ error: "invalid token" });    
-        // res.redirect("/");
+          
+        res.redirect("/");
       } else {
         const uploader = async (path) =>
         await cloudinary.uploads(path, "Images");
@@ -97,8 +97,8 @@ module.exports.profile_update = async (req, res) => {
     });
   } else {
     console.log("no token");
-    res.status(400).json({ error: "no token" });
-    // res.redirect("/login");
+    
+    res.redirect("/login");
   }
 };
 
